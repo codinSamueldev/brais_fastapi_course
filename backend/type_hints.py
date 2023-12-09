4dcd6684ff5c80e96b893a422f09239b7da2d92a
Samuel Henao <codinsamueldev@gmail.com>
2023-12-09 11:34:50 -0500

    comprehend type hints In order to FastAPI work with the declared types of variables as they should work

---
+++ backend/type_hints.py
@@ -0,0 +1,13 @@
+### Type hints ###
+
+variable = "string"
+print(variable, type(variable), "\n")
+
+variable = 15
+print(variable, type(variable))
+
+another_var: int = "My other variable"
+print(another_var, type(another_var))
+
+another_var: str = 20
+print(another_var, type(another_var))
