dc1ff6173e30932292c0708b956c8d1faf384bc3
Samuel Henao <codinsamueldev@gmail.com>
2023-12-09 12:43:01 -0500

    update tag

--- backend/FastAPI/main.py
+++ backend/FastAPI/main.py
@@ -7,3 +7,7 @@ app = FastAPI()
 @app.get('/', tags=["Root"])
 async def root():
     return JSONResponse(content={"sample text": "Holi JSON sample text en root"})
+
+@app.get('/url', tags=["URL"])
+async def url():
+    return {"url": "https://www.google.com"}
