950bdc6dcf58684bb0dfd2b36e5ac012772927d0
Samuel Henao <codinsamueldev@gmail.com>
2023-12-10 16:38:29 -0500

    ignore security

--- backend/FastAPI/main.py
+++ backend/FastAPI/main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from routers import products, users
+# import class so as to present static files, imgs.
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 # Routers
 app.include_router(products.router)
 app.include_router(users.router)
+# url: http://127.0.0.1:8000/static/imgs/name_of_the_file.jpeg
+app.mount("/static", StaticFiles(directory="static"), name="img")
 
 
 @app.get('/', tags=["Root"])
