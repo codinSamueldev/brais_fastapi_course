b66e88af2986ee4babe680f46b902af0c9dc03b6
Samuel Henao <codinsamueldev@gmail.com>
2023-12-18 20:56:29 -0500

    update

--- backend/FastAPI/db/schemas/user.py
+++ backend/FastAPI/db/schemas/user.py
@@ -2,3 +2,7 @@ def user_schema(user) -> dict:
     return {"id": str(user["_id"]),
             "username": user["username"],
             "email": user["email"]}
+
+
+def users_schema(users) -> list:
+    return [user_schema(user) for user in users]
--- backend/FastAPI/routers/users_db.py
+++ backend/FastAPI/routers/users_db.py
@@ -1,64 +1,38 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from db.schemas.user import user_schema, users_schema
+from db.models.user import User
+from db.client import db_client
+#
+from bson import ObjectId
 
 router = APIRouter(prefix="/usersdb", 
                    tags=["Users DB"], 
                    responses={404: {"message": "Not Found"}})
 
-# Base model allows to create entities.
-class User(BaseModel):
-    id: int
-    name: str
-    last_name : str
-    nickname: str
-    programmer: bool
 
-    model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "id": 999,
-                    "name": "Juan",
-                    "last_name": "Gonzales",
-                    "nickname": "juanito",
-                    "programmer": True
-                }
-            ]
-        }
-    }
+users_list = []
 
 
-users_list = [User(id=0, name="Samuel", last_name="Henao", nickname="csamueldev", programmer=True),
-         User(id=1, name="Alpaca", last_name="Gonzales", nickname="alpacor", programmer=False),
-         User(id=2, name="Alberto", last_name="Ramirez", nickname="albertico", programmer=False)]
-
-
-def get_user(id: int):
-    # For each element in user_list, filter each one if the id matches.
-    user = filter(lambda user: user.id == id, users_list)
+def search_user(field: str, key):
     try:
-        return list(user)[0]
+        # Find user's email in order to prevent have same email in the db
+        user = db_client.local.users.find_one({field: key})
+        # Return all elements in the user_schema()
+        return User(**user_schema(user))
     except:
         return JSONResponse(content={"404":"Not found"}, status_code=404)
 
 
-@router.get('/usersjson')
-async def usersjson():
-    return [{"name": "Samuel", "last name": "Henao", "nickname": "csamueldev", "Programmer": True}, 
-            {"name": "Alpaca", "last name": "Gonzales", "nickname": "alpacor", "Programmer": False}, 
-            {"name": "Alberto", "last name": "Ramirez", "nickname": "albertico", "Programmer": False}]
-
-
-@router.get('/')
+@router.get('/', response_model=list[User])
 async def users():
-    # return entity
-    return users_list
+    return users_schema(db_client.local.users.find())
 
 # Path parameter.
 @router.get('/{id}', status_code=200)
-async def user(id: int):
-    return get_user(id)
+async def user(id: str):
+    return search_user("_id", ObjectId(id))
     
 """ 
 We can retrieve information with a path parameter such as id, 
@@ -68,18 +42,29 @@ Either way, both are possible
 
 # Query parameters
 @router.get('/', status_code=200)
-async def user_query(id: int | None = None):
-    return get_user(id)
+async def user_query(id: str | None = None):
+    return search_user("_id", ObjectId(id))
 
 
 # POST
 @router.post('/', status_code=201, response_model=User)
 async def new_user(user: User):
-    if type(get_user(user.id)) == User:
+    
+    # If email is already stored, raise error.
+    if type(search_user("email", user.email)) == User:
         raise HTTPException(status_code=422, detail="User already exists")
-    else:
-        user_created = users_list.append(user)
-        return JSONResponse(status_code=201, content=user_created)
+   
+    # If not, then store new user values and delete its id
+    user_dict = dict(user)
+    del user_dict["id"]
+
+    # Store new user id in the users schema.
+    id = db_client.local.users.insert_one(user_dict).inserted_id
+
+    # Then, find id 
+    new_user = user_schema(db_client.local.users.find_one({"_id": id}))
+
+    return User(**new_user)
 
 
 # PUT
